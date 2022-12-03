e8b8b4adb14d70794d45079399cec245e178ea53
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2022-12-03 17:52:26 +0200

    feat: add popup feature in Testimonials block

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,4 +1,4 @@
-/* Hamburger menu handling */
+/* Hamburger menu handling in HEADER section**/
 let divHamburgerMenu = document.querySelector(".hamburger-menu");
 divHamburgerMenu.addEventListener("click", hamburgerMenuHandler);
 
@@ -26,4 +26,57 @@ function closeHamburgerMenu(e){
         divOverlay.classList.remove("active");
     }
 }
-/*// Hamburger menu handling */
+/*// Hamburger menu handling in HEADER section*/
+
+
+/*Popup handling in TESTIMONIALS section*/
+
+// Get the card modal windows
+let modalWindows = document.querySelectorAll(".card-modal-window"); //NodeList
+let modalWindowsArr = [... modalWindows];
+// console.log(modalWindowsArr);
+
+// Get the cards that opens the modal
+let cards = document.querySelectorAll(".testimonials-card"); //NodeList
+let cardsArr = [... cards].filter((v, index) => (index % 2) == 0);
+// console.log(cardsArr);
+
+// Get the element that closes the modal
+let modalWindowsCloseButtons = document.querySelectorAll(".modal-close-button"); //NodeList
+let modalWindowsCloseButtonsArr = [... modalWindowsCloseButtons];
+// console.log(modalWindowsCloseButtonsArr);
+
+// When the user clicks the card review, open the modal window
+cardsArr.forEach(function(card,index){
+        card.addEventListener("click", function(){
+            if(window.innerWidth < 1000){
+                modalWindowsArr[index].style.display = "block";
+            }
+        })
+});
+
+// When the user clicks on the close button(x), close the modal
+modalWindowsCloseButtonsArr.forEach(function(btn, index){
+        btn.addEventListener("click", function(){
+            if(window.innerWidth < 1000){
+                modalWindowsArr[index].style.display = "none";
+            }
+        })
+    }
+)
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    modalWindows.forEach(function(modalWindow, index){
+        if(window.innerWidth < 1000){
+            if (event.target == modalWindow) {
+                modalWindow.style.display = "none";
+            }
+        }
+    })
+}
+
+
+
+
+/*// Popup handling in TESTIMONIALS section*/
